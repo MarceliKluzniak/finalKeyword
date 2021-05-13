8b502c1f566cf1fbc4d76741a5401c53faa9b9b6
Marceli Kluzniak <marcelikluzniak5@gmail.com>
2021-05-13 08:02:21 +0200

    sample of final form

---
+++ src/pl/mkluzniak/Main.java
@@ -0,0 +1,12 @@
+package pl.mkluzniak;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final double PI = 3.14159; //everything what is final cannot be changed
+        //PI = 4; // without final form this will change the declaration of number PI
+        System.out.println(PI);
+
+
+    }
+}
